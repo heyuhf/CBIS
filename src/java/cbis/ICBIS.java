15ff2edd9b0f48e8895aca89542b5d151476e25d
yuhaifei12 <287368564@qq.com>
2013-07-26 18:15:59 +0800

    添加web服务的接口 init

--- src/java/cbis/ICBIS.java
+++ src/java/cbis/ICBIS.java
@@ -13,6 +13,24 @@ import javax.jws.WebService;
  */
 @WebService(serviceName="CBISService")
 public interface ICBIS {
+    /*
+     * test方法
+     */
     @WebMethod()
     String test();
+    
+    /*
+     * 通过店铺名字获取单个用户对象
+     */
+    @WebMethod()
+    User getUserByShopName();
+    
+    /*
+     * 通过单个或者多个标签获取用户对象列表
+     */
+    @WebMethod()
+    User[] getUserListByTags();
+    
+    
+    
 }
---
+++ src/java/cbis/Shop.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cbis;
+
+/**
+ *
+ * @author heyu
+ */
+public class Shop {
+    private String shopName;
+    private String address;
+}
